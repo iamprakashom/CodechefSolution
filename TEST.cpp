b140b17a74f719b0df9d615f772b4121438aee69
Om Prakash <iamprakashom@users.noreply.github.com>
2015-10-18 00:47:25 +0530

    TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int a;
+    while(scanf("%d",&a)){
+        if(a!=42)
+        cout<<a<<endl;
+        else
+            break;
+    }
+    return 0;
+}
